a6f30cf5ea631f9dcbd673e78673255c91130cfe
marsian8 <spanarve@gmail.com>
2022-12-23 00:50:32 +0530

    changes

--- src/components/Game.jsx
+++ src/components/Game.jsx
@@ -4,6 +4,7 @@ import { useState } from "react";
 import { useEffect } from "react";
 import { Link, useNavigate, useParams } from "react-router-dom";
 import { useAuth } from "../contexts/AuthContext";
+import GameCSS from "./Game.module.css";
 
 const Game = (props) => {
   //decode url parameters
@@ -11,6 +12,8 @@ const Game = (props) => {
 
   //useState hooks
   const [game, setGame] = useState({});
+  const [gameState, setGameState] = useState("");
+  const [opponent, setOpponent] = useState();
 
   //misc hooks
   const { currentUser } = useAuth();
@@ -18,12 +21,55 @@ const Game = (props) => {
 
   //useEffect hooks
   useEffect(() => {
+    //Load game data
     if (!currentUser) {
       navigate("/");
     }
-    //Load game data
     LoadGameData();
-  }, []);
+  }, [currentUser]);
+
+  useEffect(() => {
+    if (!game.state) {
+      return;
+    }
+    //decode the state every time game is updated
+    const decodedState = game.state.split("/");
+    if (decodedState[0] === "win") {
+      if (decodedState[1] === "p1") {
+        if (game.player1 === currentUser.username) {
+          setGameState("You win");
+        } else {
+          setGameState("They win");
+        }
+      }
+      if (decodedState[1] === "p2") {
+        if (game.player2 === currentUser.username) {
+          setGameState("You win");
+        } else {
+          setGameState("They win");
+        }
+      }
+    }
+    if (decodedState[0] === "wait") {
+      if (decodedState[1] === "p1") {
+        if (game.player1 === currentUser.username) {
+          setGameState("Your move");
+        } else {
+          setGameState("Their move");
+        }
+      }
+      if (decodedState[1] === "p2") {
+        if (game.player2 === currentUser.username) {
+          setGameState("Your move");
+        } else {
+          setGameState("Their move");
+        }
+      }
+    }
+    if (decodedState[0] === "draw") {
+      setGameState("Draw");
+    }
+  }, [game]);
 
   //functions
   const LoadGameData = async () => {
@@ -33,16 +79,20 @@ const Game = (props) => {
     }
     setGame(data.result);
     if (data.result.player1 === currentUser.username) {
-      setGame((prev) => {
-        return { ...prev, opponent: prev.player2 };
-      });
+      setOpponent(data.result.player2);
     } else if (data.result.player2 === currentUser.username) {
-      setGame((prev) => {
-        return { ...prev, opponent: prev.player1 };
-      });
+      setOpponent(data.result.player1);
     } else {
       navigate("/");
     }
+  };
+
+  const playMove = async (box) => {
+    const playChar = currentUser.username === game.player1 ? "x" : "o";
+    await axios.put(`/api/game/play/${id}/${playChar} `, {
+      box: box,
+    });
+    LoadGameData();
   };
 
   return (
@@ -50,7 +100,7 @@ const Game = (props) => {
       <Link className="btn-back" to={"/"}>
         &lsaquo;
       </Link>
-      <p className="heading">Game with {game && game.opponent}</p>
+      <p className="heading">Game with {opponent}</p>
       <p className="color-primary align-start fw-400 my-1">Your piece</p>
       <p className="game-piece">
         <img
@@ -62,6 +112,30 @@ const Game = (props) => {
           className={`game-piece`}
         />
       </p>
+      <p className={`${GameCSS.turnHint}`}>{gameState}</p>
+      <div className={`${GameCSS.board}`}>
+        {game.board &&
+          game.board.map((box, index) => {
+            const p = box.toLowerCase();
+            return (
+              <div
+                key={index}
+                className={`${GameCSS.pieceBox}`}
+                onClick={() => {
+                  playMove(index);
+                }}
+              >
+                {p === "x" ? (
+                  <img src="/cross.png"></img>
+                ) : p === "o" ? (
+                  <img src="/not.png"></img>
+                ) : (
+                  <></>
+                )}
+              </div>
+            );
+          })}
+      </div>
     </>
   );
 };
